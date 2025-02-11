5e2bbb280ebe88c8b419d4ee1f881d2997f4666d
P Jaworski <pjaworski.dev@gmail.com>
2025-02-11 22:11:32 +0100

    fixed price type check

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -20,6 +20,10 @@ export const addNewProduct = async (req, res) => {
         return res.status(400).json({success: false, message: "Please provide all fields"})
     }
 
+    if(isNaN(product.price)) {
+        return res.status(400).json({success: false, message: "Provided price type is not a number."});
+    }
+
     const newProduct = new Product(product);
 
     try {
@@ -34,7 +38,10 @@ export const addNewProduct = async (req, res) => {
 
 export const deleteProductById = async (req, res) => {
     const {id} = req.params;
-    if(!id) return res.json(400).json({success: false, message: "ID (params) is required"})
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({success: false, message: `Invalid product ID`});
+    }
+
     try {
         await Product.findByIdAndDelete(id);
         res.status(200).json({success: true, message: "Deleted " + id})
